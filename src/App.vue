86c10f45cf686025816967d41755ca887512fb6d
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2021-09-25 05:42:29 +0300

    before sass loader installed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <MushBattle />
+
 </template>
 
+
+
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MushBattle from './components/MushBattle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MushBattle
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
+
+
+<style lang="sass">
+
+#app
+  font-family: monospace
+
 </style>
---
+++ src/components/MushBattle.vue
@@ -0,0 +1,49 @@
+<template>
+
+<text>mushroom sea battle</text>
+
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+export default {
+
+  name: 'MushBattle',
+
+  props: {
+
+  }
+
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+<style scoped lang="sass">
+
+text
+  color: red
+
+</style>
